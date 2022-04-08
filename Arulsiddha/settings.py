caf5cd0dcd34a87d65f0ffbee38a05d5429e30c0
kavyakrishnan <kavyayasotha@gmail.com>
2022-04-08 19:25:24 +0530

    'settingscommanded'

--- Arulsiddha/settings.py
+++ Arulsiddha/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import django_heroku
-import dj_database_url
-from decouple import config
+# import dj_database_url
+# from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
